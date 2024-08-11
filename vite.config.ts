c3421d7811a5fa530b810908259f0e6f7f542204
UeberTimei <super.timati04@gmail.com>
2024-08-11 23:53:41 +0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/github-graphql-api-table_app/",
+  base: "github-grpaphql-api",
 });
